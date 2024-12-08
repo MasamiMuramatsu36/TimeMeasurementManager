d65aad7ffbb3eafe877e26fcaa8041cf98601cf6
MasamiMuramatsu36 <muramasa36project@gmail.com>
2024-12-08 20:25:55 +0900

    仮実装
    TODO：
    ・ 指定した名前の計測器を開始・停止できるようにする。
    ・ テストプログラムの作成。

---
+++ Dll/StopWatchManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dll
+{
+    /// <summary>
+    /// 計測器を管理します
+    /// </summary>
+    public static class StopWatchManager
+    {
+        /// <summary>
+        /// 最上位の計測器
+        /// </summary>
+        private static StopWatchNode RootStopWatch { get; set; }
+        
+        /// <summary>
+        /// 最上位の計測器を設定します
+        /// </summary>
+        /// <param name="rootStopWatch"></param>
+        public static void SetRootStopWatch(StopWatchNode rootStopWatch)
+        {
+            RootStopWatch = rootStopWatch;
+        }
+
+        /// <summary>
+        /// 指定した計測器を子として追加します
+        /// </summary>
+        /// <param name="stopWatchNode"></param>
+        public static void AddChildStopWatch(StopWatchNode stopWatchNode)
+        {
+            RootStopWatch.AddChildStopWatch(stopWatchNode);
+        }
+
+        /// <summary>
+        /// 指定した名前の計測器に、指定した計測器を追加します
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="stopWatchNode"></param>
+        /// <returns></returns>
+        public static bool TryAddStopWatch(string name, StopWatchNode stopWatchNode)
+        {
+            if (!RootStopWatch.HasStopWatch(name)) { return false; }
+
+
+        }
+
+        /// <summary>
+        /// 自身の計測器の情報を取得します
+        /// </summary>
+        /// <returns></returns>
+        public static string GetRootStopWatchInfo()
+        {
+            return $"{RootStopWatch.GetCurrentStopWatchInfo()}";
+        }
+
+        /// <summary>
+        /// 所有する計測器の情報を取得します
+        /// </summary>
+        /// <returns></returns>
+        public static string GetChilsStopWatchInfo()
+        {
+            return RootStopWatch.GetChilsStopWatchInfo();
+        }
+
+        /// <summary>
+        /// 自身も含めた所有するすべての計測器の情報を取得します
+        /// </summary>
+        /// <returns></returns>
+        public static string GetAllStopWatchInfo()
+        {
+            return RootStopWatch.GetAllStopWatchInfo();
+        }
+    }
+}
---
+++ Dll/StopWatchNode.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dll
+{
+    /// <summary>
+    /// 計測器を表します
+    /// </summary>
+    public class StopWatchNode
+    {
+        /// <summary>
+        /// 識別名
+        /// </summary>
+        private string IdentifyName { get; set; }
+
+        /// <summary>
+        /// 計測器
+        /// </summary>
+        private Stopwatch CurrentStopWatch { get; set; }
+
+        /// <summary>
+        /// 子として持つ計測器
+        /// </summary>
+        private Collection<StopWatchNode> ChildStopWatch { get; set; }
+
+        #region コンストラクタ
+
+        /// <summary>
+        /// 計測器を作成します
+        /// </summary>
+        public StopWatchNode(string identifyName) : this(identifyName, new Stopwatch(), new Collection<StopWatchNode>())
+        {
+        }
+
+        /// <summary>
+        /// 計測器を作成します
+        /// </summary>
+        /// <param name="stopWatch"></param>
+        public StopWatchNode(string identifyName, Stopwatch stopWatch) : this(identifyName, stopWatch, new Collection<StopWatchNode>()) 
+        {
+        }
+
+        /// <summary>
+        /// 計測器を作成します
+        /// </summary>
+        /// <param name="childStopWatch"></param>
+        public StopWatchNode(string identifyName, Collection<StopWatchNode> childStopWatch) : this(identifyName, new Stopwatch(), childStopWatch)
+        {
+        }
+
+        /// <summary>
+        /// 計測器を作成します
+        /// </summary>
+        /// <param name="stopWatch"></param>
+        /// <param name="ChildStopWatch"></param>
+        public StopWatchNode(string identifyName, Stopwatch stopWatch, Collection<StopWatchNode> childStopWatch)
+        {
+            this.IdentifyName = identifyName;
+            this.CurrentStopWatch = stopWatch;
+            this.ChildStopWatch = childStopWatch;
+        }
+
+        #endregion // コンストラクタ
+
+        public void StartStopWatch()
+        {
+            this.CurrentStopWatch = Stopwatch.StartNew();
+        }
+
+        public void StopStopWatch()
+        {
+            this.CurrentStopWatch.Stop();
+        }
+
+        /// <summary>
+        /// 指定した計測器を子として追加します
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="stopwatch"></param>
+        public void AddChildStopWatch(StopWatchNode stapWatchNode)
+        {
+            this.ChildStopWatch.Add(stapWatchNode);
+        }
+
+        /// <summary>
+        /// 指定した名前の計測器に、指定した計測器を追加します
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="stopWatchNode"></param>
+        /// <returns></returns>
+        public bool TryAddStopWatch(string name, StopWatchNode stopWatchNode)
+        {
+            if (!this.HasStopWatch(name)) { return false; }
+
+            if (this.IdentifyName == name)
+            {
+                this.AddChildStopWatch(stopWatchNode);
+            }
+            else
+            {
+                foreach (var csw in this.ChildStopWatch)
+                {
+                    csw.TryAddStopWatch(name, stopWatchNode);
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 指定した名前の計測器を持っているかを確かめます
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public bool HasStopWatch(string name)
+        {
+            return this.IdentifyName == name ||
+                this.ChildStopWatch.Any(x => x.HasStopWatch(name));
+        }
+
+        /// <summary>
+        /// 計測器の情報を取得します
+        /// </summary>
+        /// <returns></returns>
+        public string GetCurrentStopWatchInfo()
+        {
+            return $"{this.IdentifyName} : {this.CurrentStopWatch.Elapsed}";
+        }
+
+        /// <summary>
+        /// 子の計測器の情報を取得します
+        /// </summary>
+        /// <returns></returns>
+        public string GetChilsStopWatchInfo()
+        {
+            return string.Join("\r\n", this.ChildStopWatch.Select(x => x.GetCurrentStopWatchInfo()).ToArray());
+        }
+
+        /// <summary>
+        /// 子も含めたすべての計測器の情報を取得します
+        /// </summary>
+        /// <returns></returns>
+        public string GetAllStopWatchInfo()
+        {
+            return $"{GetCurrentStopWatchInfo()}\r\n{GetChilsStopWatchInfo()}";
+        }
+    }
+}
